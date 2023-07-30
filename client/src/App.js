a5581f62ee0b76672e98b337a2cc4a4c952ef0f3
unknown <goluchoudhary7808@gmail.com>
2023-07-30 13:27:59 +0530

    addloginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,9 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import "./App.css";
+import React from 'react'
 
 function App() {
-	const [user, setUser] = useState(null);
-
-	const getUser = async () => {
-		try {
-			const url = `https://googleoauth2o.onrender.com/auth/login/success`;
-			const { data } = await axios.get(url, { withCredentials: true });
-			setUser(data.user._json);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	useEffect(() => {
-		getUser();
-	}, []);
-
-	return (
-		<div className="container">
-			<Routes>
-				<Route
-					exact
-					path="/"
-					element={user ? <Home user={user} /> : <Navigate to="/login" />}
-				/>
-				<Route
-					exact
-					path="/login"
-					element={user ? <Navigate to="/" /> : <Login />}
-				/>
-				<Route
-					path="/signup"
-					element={user ? <Navigate to="/" /> : <Signup />}
-				/>
-			</Routes>
-		</div>
-	);
+  return (
+	<div>App</div>
+  )
 }
 
-export default App;
+export default App
