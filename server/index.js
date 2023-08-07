af9880b292dd280b89c5c99a37b0a54f9beef7c5
unknown <goluchoudhary7808@gmail.com>
2023-08-07 16:51:24 +0530

    add

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "https://googleouth.vercel.app",
+		origin: "http://localhost:3000",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
