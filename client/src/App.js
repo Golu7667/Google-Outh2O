65ca5ff1c1e385775affb974ee780e36963814cd
unknown <goluchoudhary7808@gmail.com>
2023-08-07 16:23:18 +0530

    add correct import

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import {Routes,Route} from 'react-router-dom'
+
 
 function App() {
 	const [user, setUser] = useState(null);
